a5e12dcf58792741c7f42ed44aec0433719507da
ksk192830 <ksk192830@g.skku.edu>
2026-01-25 14:59:51 +0900

    state에 따른 노드 실행 설정 추가

--- src/control_pkg/launch/final_parking.py
+++ src/control_pkg/launch/final_parking.py
@@ -22,9 +22,31 @@ def generate_launch_description():
                 # {'data_source': 'camera'},  # camera, video, image 선택
 
                 {'cam_num': 2},
-                {'img_dir': '/home/sg/gyeonggi_ws/src/camera_pkg/camera_pkg/lib/parking_front_new'},
+                {'img_dir': '/home/sg/gyeonggi_ws/src/camera_pkg/camera_pkg/lib/parking_front_perfect(slot2)'},
                 {'pub_topic': '/cam0/image_raw'},
                 {'window_name': 'Raw 0'},
+                {'show_image': False},
+
+                # {'timer_period': 0.03},  # float형으로, fps조절 
+                {'timer_period': 0.5},
+            ]
+        ),
+
+        #################### CAMERA2(후방) ######################
+        Node(
+            package='camera_pkg',
+            executable='image',  # 실행할 노드 파일명
+            name='cam1',
+            namespace='cam1',
+            parameters=[
+                {'data_source': 'image'},
+                # {'data_source': 'camera'},  # camera, video, image 선택
+                # {'rotate_mode': 1},
+
+                {'cam_num': 4},
+                {'img_dir': '/home/sg/gyeonggi_ws/src/camera_pkg/camera_pkg/lib/parking_rear_perfect(slot2)'},
+                {'pub_topic': '/cam1/image_raw'},
+                {'window_name': 'Raw 1'},
                 {'show_image': False},
 
                 # {'timer_period': 0.03},  # float형으로, fps조절 
@@ -44,7 +66,10 @@ def generate_launch_description():
                 # cpu or cuda:0
                 {'device': 'cuda:0'},
                 {'model_path': cam0_model_path},
-                {'threshold': 0.5}
+                {'threshold': 0.5},
+
+                # segmentation 수행할 state 목록
+                {'allowed_states': ['SEARCHING_SPACE', 'TURN_RIGHT']}
             ],
             remappings=[
                 ('image_raw', '/cam0/image_raw'),  
@@ -67,27 +92,6 @@ def generate_launch_description():
         ),
         
 
-        #################### CAMERA2(후방) ######################
-        Node(
-            package='camera_pkg',
-            executable='image',  # 실행할 노드 파일명
-            name='cam1',
-            namespace='cam1',
-            parameters=[
-                {'data_source': 'image'},
-                # {'data_source': 'camera'},  # camera, video, image 선택
-                # {'rotate_mode': 1},
-
-                {'cam_num': 4},
-                {'img_dir': '/home/sg/gyeonggi_ws/src/camera_pkg/camera_pkg/lib/parking_rear_new'},
-                {'pub_topic': '/cam1/image_raw'},
-                {'window_name': 'Raw 1'},
-                {'show_image': False},
-
-                # {'timer_period': 0.03},  # float형으로, fps조절 
-                {'timer_period': 0.5},
-            ]
-        ),
         
         #################### CAMERA2 YOLO SEG ######################
         SetEnvironmentVariable('CUDA_VISIBLE_DEVICES', '0'),
@@ -101,7 +105,10 @@ def generate_launch_description():
                 # cpu or cuda:0
                 {'device': 'cuda:0'},
                 {'model_path': cam1_model_path},
-                {'threshold': 0.5}
+                {'threshold': 0.5},
+
+                # segmentation 수행할 state 목록
+                {'allowed_states': ['ALIGN_TO_SPACE', 'PARKING_IN']}
             ],
             remappings=[
                 ('image_raw', '/cam1/image_raw'),  
@@ -123,33 +130,34 @@ def generate_launch_description():
             }],
         ),
 
-        #################### PARKING MAP VIZ ######################
-        Node(
-            package="decision_making_pkg",
-            executable="parking_map_viz",   # ← 노드 파일명
-            name="parking_map_viz",
-            output="screen",
-            parameters=[{
-                "yaml_path": "/home/sg/gyeonggi_ws/src/decision_making_pkg/decision_making_pkg/config/parking_map.yaml",
-                "frame_id": "map",
-                "topic": "/parking_map/markers",
-
-                "show_parking_lot": True,
-                "selected_slot_id": "slot_02",   # "" 이면 전체 표시
-            }]
-        ),
-
-        # #################### LIDAR SCAN ######################
+        # #################### PARKING MAP VIZ ######################
         # Node(
-        #     package='lidar_pkg',              # 라이다 패키지 이름
-        #     executable='scan',          # setup.cfg에 등록한 이름 사용!
-        #     name='lidar_scan',
-        #     output='screen',
-        #     parameters=[
-        #         {'pub_topic': '/scan_raw'},    # MotionNode에서 구독하는 토픽 이름과 맞추기
-        #         {'lidar_port': '/dev/ttyUSB0'},
-        #     ]
+        #     package="decision_making_pkg",
+        #     executable="parking_map_viz",   # ← 노드 파일명
+        #     name="parking_map_viz",
+        #     output="screen",
+        #     parameters=[{
+        #         "yaml_path": "/home/sg/gyeonggi_ws/src/decision_making_pkg/decision_making_pkg/config/parking_map.yaml",
+        #         "frame_id": "map",
+        #         "topic": "/parking_map/markers",
+
+        #         "show_parking_lot": True,
+        #         "selected_slot_id": "slot_02",   # "" 이면 전체 표시
+        #     }]
         # ),
+
+        #################### LIDAR SCAN ######################
+        Node(
+            package='lidar_pkg',              # 라이다 패키지 이름
+            executable='scan',          # setup.cfg에 등록한 이름 사용!
+            name='lidar_scan',
+            output='screen',
+            parameters=[
+                {'pub_topic': '/scan_raw'},    # MotionNode에서 구독하는 토픽 이름과 맞추기
+                {'lidar_port': '/dev/ttyUSB0'},
+                {'allowed_states': ['ALIGN_TO_SPACE']},
+            ]
+        ),
         # #################### LIDAR Cluster ######################
         # Node(
         #     package='lidar_pkg',          # your package name
